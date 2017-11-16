c623a45245c06be192a8267b4c6278f3737d77b6
Gilles Dubois <gilles.dubois@factorfx.com>
2017-11-16 17:38:48 +0100

    Update headers

--- install.php
+++ install.php
@@ -1,25 +1,26 @@
 <?php
 /*
- * Copyright 2005-2016 OCSInventory-NG/OCSInventory-ocsreports contributors.
+ * Copyright 2005-2017 PluginsOCSInventory-NG/vveeam-enterprise-manager contributors.
  * See the Contributors file for more details about them.
  *
- * This file is part of OCSInventory-NG/OCSInventory-ocsreports.
+ * This file is part of PluginsOCSInventory-NG/vveeam-enterprise-manager.
  *
- * OCSInventory-NG/OCSInventory-ocsreports is free software: you can redistribute
+ * PluginsOCSInventory-NG/vveeam-enterprise-manager is free software: you can redistribute
  * it and/or modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the License,
+ * published by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
  *
- * OCSInventory-NG/OCSInventory-ocsreports is distributed in the hope that it
+ * PluginsOCSInventory-NG/vveeam-enterprise-manager is distributed in the hope that it
  * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with OCSInventory-NG/OCSInventory-ocsreports. if not, write to the
+ * along with PluginsOCSInventory-NG/vmware-vcenter. if not, write to the
  * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
  */
+
 
 function plugin_version_veeam()
 {
--- ms_veeam.php
+++ ms_veeam.php
@@ -1,16 +1,16 @@
 <?php
 /*
- * Copyright 2005-2017 PluginsOCSInventory-NG/vmware-vcenter contributors.
+ * Copyright 2005-2017 PluginsOCSInventory-NG/vveeam-enterprise-manager contributors.
  * See the Contributors file for more details about them.
  *
- * This file is part of PluginsOCSInventory-NG/vmware-vcenter.
+ * This file is part of PluginsOCSInventory-NG/vveeam-enterprise-manager.
  *
- * PluginsOCSInventory-NG/vmware-vcenter is free software: you can redistribute
+ * PluginsOCSInventory-NG/vveeam-enterprise-manager is free software: you can redistribute
  * it and/or modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
  *
- * PluginsOCSInventory-NG/vmware-vcenter is distributed in the hope that it
+ * PluginsOCSInventory-NG/vveeam-enterprise-manager is distributed in the hope that it
  * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
